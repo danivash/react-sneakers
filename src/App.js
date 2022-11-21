b74bb3ec627c010e69aa72badca73e0d00e50476
danivash <03danivas@gmail.com>
2022-11-21 23:53:29 +0100

    create right basket

--- src/App.js
+++ src/App.js
@@ -2,13 +2,55 @@ import './index.scss';
 import Header from "./components/Header/Header";
 import Card from './components/Card/Card';
 import Search from './images/content/search.svg';
-
+import Vector from './images/content/Vector.svg';
+import Sneakers from './images/sneakers/nike_green.jpg'
 function App() {
   return (
     <div className="wrapper">
+      <div className="overlay">
+        <div className="right-wrapper">
+          <h2>Basket</h2>
+
+          <div className="items">
+            <div className="basketItem">
+              <img width={70} height={70} src={Sneakers} alt="Sneakers" />
+              <div style={{ marginLeft: "15px" }}>
+                <p>Man Sneakers: Nike Blazer Mid Suede</p>
+                <b>45$</b>
+              </div>
+              <button>
+                <img src={Vector} alt="Remove" />
+              </button>
+            </div>
+            <div className="basketItem">
+              <img width={70} height={70} src={Sneakers} alt="Sneakers" />
+              <div style={{ marginLeft: "15px" }}>
+                <p>Man Sneakers: Nike Blazer Mid Suede</p>
+                <b>45$</b>
+              </div>
+              <button>
+                <img src={Vector} alt="Remove" />
+              </button>
+            </div>
+          </div>
+
+          <ul>
+            <li>
+              <span>Total:</span>
+              <div></div>
+              <b>45$</b>
+            </li>
+            <li>
+              <span>Charge 10%:</span>
+              <div></div>
+              <b>4$</b>
+            </li>
+          </ul>
+        </div>
+      </div>
       <Header />
       <div className="content">
-        <div className="content-header" style={{marginBottom: "40px"}}>
+        <div className="content-header" style={{ marginBottom: "40px" }}>
           <h1 style={{ margin: "0" }}>All sneakers</h1>
           <div className="search-block">
             <img src={Search} alt="Search" />
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,22 +5,22 @@ import Sneakers1 from "../../images/sneakers/nike_green.jpg"
 import Favorite from "../../images/content/card/favorite.jpg"
 const Card = () => {
   return (
-        <div className={classes.card}>
-        <button className={classes.button}>
-            <img src={Favorite} alt="Save Sneakers to favorite" />
-          </button>
-          <img src={Sneakers1} width={133} height={112} alt="Sneakers" />
-          <h5>Man Sneakers: Nike Blazer Mid Suede</h5>
-          <div className={classes.card_footer}>
-            <div className={classes.card_price}>
-              <span>Price:</span>
-              <b>45$</b>
-            </div>
-            <button className={classes.button}>
-              <img src={Plus} width={11} height={11} alt="plus" />
-            </button>
-          </div>
+    <div className={classes.card}>
+      <button className={classes.button}>
+        <img src={Favorite} alt="Save Sneakers to favorite" />
+      </button>
+      <img src={Sneakers1} width={133} height={112} alt="Sneakers" />
+      <h5>Man Sneakers: Nike Blazer Mid Suede</h5>
+      <div className={classes.card_footer}>
+        <div className={classes.card_price}>
+          <span>Price:</span>
+          <b>45$</b>
         </div>
+        <button className={classes.button}>
+          <img src={Plus} width={11} height={11} alt="plus" />
+        </button>
+      </div>
+    </div>
   );
 }
 
